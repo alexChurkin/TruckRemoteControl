0ff3df6659c5e9362a06c9fe560c64c1313156dc
Alex Churkin <churkindev@gmail.com>
2019-11-12 21:44:04 +0300

    Billing implemented

--- app/src/main/java/com/alexchurkin/truckremote/SettingsFragment.java
+++ app/src/main/java/com/alexchurkin/truckremote/SettingsFragment.java
@@ -5,19 +5,39 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.StringRes;
 import androidx.appcompat.app.AlertDialog;
 import androidx.preference.PreferenceFragmentCompat;
 import androidx.preference.PreferenceManager;
 
+import com.android.billingclient.api.BillingClient;
+import com.android.billingclient.api.BillingClientStateListener;
+import com.android.billingclient.api.BillingFlowParams;
+import com.android.billingclient.api.BillingResult;
+import com.android.billingclient.api.ConsumeParams;
+import com.android.billingclient.api.Purchase;
+import com.android.billingclient.api.PurchasesUpdatedListener;
+import com.android.billingclient.api.SkuDetails;
+import com.android.billingclient.api.SkuDetailsParams;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Objects;
 
-public class SettingsFragment extends PreferenceFragmentCompat {
+public class SettingsFragment extends PreferenceFragmentCompat implements PurchasesUpdatedListener, BillingClientStateListener {
+
+    private static final String SKU_SUPPORT_AUTHOR_ID = "support_the_author";
 
     private static final String KEY_SHOWED_ABOUT = "ShowedAbout";
     private boolean showedAbout;
+
+    private BillingClient mBillingClient;
+    private HashMap<String, SkuDetails> mSkuDetailsMap = new HashMap<>();
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
@@ -28,6 +48,9 @@ public class SettingsFragment extends PreferenceFragmentCompat {
                 showDialogAbout();
             }
         }
+        mBillingClient = BillingClient.newBuilder(getContext())
+                .enablePendingPurchases().setListener(this).build();
+        mBillingClient.startConnection(this);
     }
 
     @Override
@@ -46,7 +69,7 @@ public class SettingsFragment extends PreferenceFragmentCompat {
             return true;
         });
         preferenceManager.findPreference("support").setOnPreferenceClickListener(preference -> {
-
+            launchBilling();
             return true;
         });
     }
@@ -70,5 +93,83 @@ public class SettingsFragment extends PreferenceFragmentCompat {
                 .setOnDismissListener(dialogInterface -> showedAbout = false)
                 .create()
                 .show();
+    }
+
+    private void launchBilling() {
+        BillingFlowParams flowParams = BillingFlowParams.newBuilder()
+                .setSkuDetails(mSkuDetailsMap.get(SKU_SUPPORT_AUTHOR_ID))
+                .build();
+        mBillingClient.launchBillingFlow(getActivity(), flowParams);
+    }
+
+    @Override
+    public void onBillingSetupFinished(BillingResult billingResult) {
+        if (billingResult.getResponseCode() == BillingClient.BillingResponseCode.OK) {
+            querySkuDetails();
+            List<Purchase> purchases = queryPurchases();
+            for (Purchase purchase : purchases) {
+                //and not purchased value (in prefs)
+                if (purchase.getSku().equals(SKU_SUPPORT_AUTHOR_ID)) {
+                    showToast(R.string.purchase_restored);
+                    getPreferenceManager().findPreference("support").setVisible(false);
+                    //TODO Restore purchase
+                }
+            }
+        }
+    }
+
+    @Override
+    public void onBillingServiceDisconnected() {
+    }
+
+    @Override
+    public void onPurchasesUpdated(BillingResult billingResult, @Nullable List<Purchase> purchases) {
+        switch (billingResult.getResponseCode()) {
+            case BillingClient.BillingResponseCode.OK:
+                for (Purchase purchase : purchases) {
+                    if (purchase.getSku().equals(SKU_SUPPORT_AUTHOR_ID)) {
+                        ConsumeParams consParams = ConsumeParams.newBuilder()
+                                .setPurchaseToken(purchase.getPurchaseToken())
+                                .build();
+                        mBillingClient.consumeAsync(consParams, (billResult, purchaseToken) -> {
+                            showToast(R.string.purchase_success);
+                            getPreferenceManager().findPreference("support").setVisible(false);
+                            //TODO remove ad in prefs and remove item from settings
+                        });
+                    }
+                }
+                break;
+            case BillingClient.BillingResponseCode.USER_CANCELED:
+                showToast(R.string.purchase_cancelled);
+                break;
+        }
+    }
+
+    private void querySkuDetails() {
+        ArrayList<String> skuList = new ArrayList<>();
+        skuList.add(SKU_SUPPORT_AUTHOR_ID);
+
+        SkuDetailsParams.Builder params = SkuDetailsParams.newBuilder()
+                .setSkusList(skuList)
+                .setType(BillingClient.SkuType.INAPP);
+
+        mBillingClient.querySkuDetailsAsync(params.build(), (billingResult, skuDetailsList) -> {
+            if (billingResult.getResponseCode() == BillingClient.BillingResponseCode.OK) {
+                for (SkuDetails skuDetails : skuDetailsList) {
+                    mSkuDetailsMap.put(skuDetails.getSku(), skuDetails);
+                }
+            }
+        });
+    }
+
+    private List<Purchase> queryPurchases() {
+        Purchase.PurchasesResult purchasesResult =
+                mBillingClient.queryPurchases(BillingClient.SkuType.INAPP);
+        return purchasesResult.getPurchasesList();
+    }
+
+    private void showToast(@StringRes int resId) {
+        Toast toast = Toast.makeText(getContext(), resId, Toast.LENGTH_SHORT);
+        toast.show();
     }
 }
