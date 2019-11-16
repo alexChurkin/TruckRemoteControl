1617e0c09eceb6df43508ae42b0fade5476111d2
Alex Churkin <churkindev@gmail.com>
2019-11-16 10:08:14 +0300

    Guide implementation started; play console background changed

---
+++ app/src/main/java/com/alexchurkin/truckremote/activity/GuideActivity.java
@@ -0,0 +1,17 @@
+package com.alexchurkin.truckremote.activity;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.alexchurkin.truckremote.R;
+
+public class GuideActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guide);
+    }
+}
